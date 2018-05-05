276f1c1e0ab6a0c1a671e971fc2bfe1662ecc515
Rolo96 <rarias1820@live.com>
2018-05-05 02:07:37 +0200

    API Correo Lista

---
+++ MailAPI/MailAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,71 @@
+﻿using System.Data;
+using System.Linq;
+using System.Net.Mail;
+using System.Timers;
+using System.Web.Http;
+using MailAPI.Controllers;
+using MailAPI.Models;
+
+/// <summary>
+/// Inicializa el API, pone a correr el timer que revisa en background si hay mensajes nuevos y los envía
+/// </summary>
+namespace MailAPI
+{
+    public static class WebApiConfig
+    {
+        static SmtpClient server = new SmtpClient("smtp.gmail.com", 587);//SmtpGmail
+
+        /// <summary>
+        /// Constructor del API, inicializa el timer, configura el smtp
+        /// </summary>
+        /// <param name="config"></param>
+        public static void Register(HttpConfiguration config)
+        {
+            //Configuracion del smtp con la informacion del correo a utilizar
+            server.Credentials = new System.Net.NetworkCredential("notificacioneshb.ce@gmail.com", "hbCE2018");
+            server.EnableSsl = true;
+
+            //Creacion y configuracion del timer
+            Timer aTimer = new Timer(60 * 1000);//Tiempo en milisegundos para volver a ejecutar la tarea
+            aTimer.Elapsed += new ElapsedEventHandler(EnviarCorreos);
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+
+            //Configuracion general del API
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+
+        /// <summary>
+        /// Busca si hay correos a enviar y los envia por prioridad, si falla el envío
+        /// lo intenta hasta 5 veces si no lo puede enviar lo marca como fallido
+        /// </summary>
+        public static void EnviarCorreos(object source, ElapsedEventArgs e)
+        {
+            using (NotificacionesEntities entities = new NotificacionesEntities())
+            {
+                while (MailController.bandera){//Mientras hayan correos a enviar
+                    try
+                    {
+                        var mails = entities.MENSAJENUEVO.Select(p => new { p.EmailDestino, p.EmailOrigen, p.Tema, p.Mensaje, p.Prioridad, p.Intentos, p.Id }).
+                            OrderByDescending((x) => x.Prioridad).ToList();//Selecciona los correos a enviar ordenados por prioridad
+                        if (mails.Count < 1) {MailController.bandera = false; break;}//Si no hay correos a enviar se detiene
+                        var mail = mails.First();//Obtiene el mail mas prioritario
+                        try{
+                            server.Send(mail.EmailOrigen, mail.EmailDestino,
+                                mail.Tema, mail.Mensaje);//Envia el correo
+                            //Si se envió lo mueve a la tabla de correos enviados o fallidos y le pone estado enviado
+                            entities.MoveMessage(mail.Id, 1);}catch (SmtpFailedRecipientException){//Si no se puede enviar
+                            //Si ya cumplió los 5 intentos lo mueve a la tabla de correos enviados o fallidos y le pone estado fallido
+                            if (mail.Intentos >= 5) entities.MoveMessage(mail.Id, 0);
+                            else entities.IncrementarIntentos(mail.Id);}//Si no lleva los 5 intentos le suma un intento
+                    }catch (DataException){break;}
+                }
+            }
+        }
+    }
+}
---
+++ MailAPI/MailAPI/Controllers/MailController.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MailAPI.Models;
+using System.Data;
+
+/// <summary>
+/// Clase que se encarga de recibir la solicitud de enviar un correo nuevo
+/// </summary>
+namespace MailAPI.Controllers
+{
+    public class MailController : ApiController
+    {
+
+        //Bandera que se activa cuando se recibe la solicitud de enviar un correo nuevo, esto con el fin de 
+        //evitar consultas a la base de datos cuando no se reciben solicitudes de enviar correos. (usada en WebApiConfig.cs)
+        public static bool bandera = true;
+
+        /// <summary>
+        /// Recibe la solicitud de enviar un nuevo correo
+        /// </summary>
+        /// <param name="email">Json con el formato {EmailOrigen: "origen", EmailDestino: "destino", Tema: "Asunto del correo", 
+        /// Mensaje: "mensaje a enviar", Prioridad:int(mayor numero mayor prioridad)}</param>
+        /// <returns>HttpStatusCode.OK si se guardo correctamente la solicitud, HttpStatusCode.Conflict si no se pudo guardar</returns>
+        [HttpPost]
+        [Route("nuevoEmail")]
+        public HttpResponseMessage NuevoEmail(MENSAJENUEVO email)
+        {
+            using (NotificacionesEntities entities = new NotificacionesEntities())
+            {
+                if (ModelState.IsValid){
+                    try{
+                        entities.NewMessage(email.EmailOrigen, email.EmailDestino, email.Tema,
+                            email.Mensaje, 0, email.Prioridad);//Almacena la solicitud
+                        bandera = true;
+                        return Request.CreateResponse(HttpStatusCode.OK);
+                    }catch (DataException) { return Request.CreateResponse(HttpStatusCode.Conflict); }
+                }
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+    }
+}
